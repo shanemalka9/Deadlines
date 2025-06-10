0a8002a6b22491723d3bd47a571ca79aacd45527
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-06-10 19:48:10 +0530

    feat:- added the app logo

--- app/src/main/java/lk/shan/deadlines/MainActivity.kt
+++ app/src/main/java/lk/shan/deadlines/MainActivity.kt
@@ -1,42 +1,77 @@
 package lk.shan.deadlines
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import lk.shan.deadlines.screen.LoggingScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
-
-            NavHost(
-                navController = navController,
-                startDestination = "home"
-            ) {
-                composable("home") { HomeScreen(navController) }
-            }
-
+            LoggingPage()
         }
+    }
+}
+@Composable
+fun LoggingPage() {
+    Column(
+    ) {
+        ResponsiveLogo()
+        Spacer(modifier = Modifier.height(100.dp))
     }
 }
 
 @Composable
-fun HomeScreen(navController: NavHostController) {
-
-
+fun ResponsiveLogo() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.frame_16),
+            contentDescription = "App Logo",
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 120.dp) // Adds top spacing
+                .size(180.dp) // Replace with desired default or responsive logic
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    HomeScreen(navController = rememberNavController())
+fun Button(){
+
 }
+
+
+
+
