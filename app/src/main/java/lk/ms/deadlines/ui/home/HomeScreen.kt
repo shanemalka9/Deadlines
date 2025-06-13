a96dd0f7399d506be30e092019263c0164aad783
shanemalka9 <shanemalka@outlook.com>
2025-06-13 09:41:50 +0530

    Added list of events, created model and made lambda to view events

--- app/src/main/java/lk/ms/deadlines/ui/home/HomeScreen.kt
+++ app/src/main/java/lk/ms/deadlines/ui/home/HomeScreen.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -21,15 +23,17 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import lk.ms.deadlines.model.Event
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
@@ -41,9 +45,11 @@ import java.time.temporal.ChronoUnit
  * TODO: Add animation to long event names so it has a side scroll animation
  */
 
+
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun HomeScreen(){
+fun HomeScreen() {
+    val eventList = remember { mutableStateListOf<Event>() }
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -73,10 +79,10 @@ fun HomeScreen(){
                 color = Color.Black,
                 modifier = Modifier
                     .fillMaxHeight(0.6f)
-                    .padding(0.dp,40.dp,0.dp,0.dp)
+                    .padding(0.dp, 40.dp, 0.dp, 0.dp)
             )
         }
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .weight(1f)
                 .padding(0.dp, 10.dp),
@@ -84,16 +90,14 @@ fun HomeScreen(){
             verticalArrangement = Arrangement.Top,
         ) {
             // Events
-            EventIndicator(
-                eventName = "event1",
-                eventRisk = "MEDIUM",
-                timeLeft = "30 days left",
-                onClick = {/*wde*/ })
-            EventIndicator(
-                eventName = "event1",
-                eventRisk = "LOW",
-                timeLeft = "30 days left",
-                onClick = {/*wde*/ })
+            items(eventList) { event ->
+                EventIndicator(
+                    eventName = event.name,
+                    eventRisk = event.priority,
+                    timeLeft = event.endDateTime.toString(),
+                    onClick = { /* Handle click */ }
+                )
+            }
         }
         Row(
             modifier = Modifier
@@ -103,7 +107,7 @@ fun HomeScreen(){
             verticalAlignment = Alignment.CenterVertically
         ) {
             Button(
-                onClick = {/*TODO*/},
+                onClick = {/*TODO*/ },
                 shape = RoundedCornerShape(50.dp)
             ) {
                 Text(
@@ -121,10 +125,10 @@ fun HomeScreen(){
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun EventIndicator(
-    eventName:String,
-    eventRisk:String,
-   // riskColur:String
-    timeLeft:String,// only for now need to be change the time later
+    eventName: String,
+    eventRisk: String,
+    // riskColur:String
+    timeLeft: String,// only for now need to be change the time later
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 
@@ -145,11 +149,11 @@ fun EventIndicator(
             pressedElevation = 12.dp
         )
     ) {
-        Column (
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(0.dp)
-        ){
+        ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth(),
@@ -173,7 +177,7 @@ fun EventIndicator(
 
             }
             Spacer(modifier = Modifier.height(10.dp)) // Small gap between details and progress bar
-            Row(modifier = Modifier.fillMaxWidth()){
+            Row(modifier = Modifier.fillMaxWidth()) {
                 DeadlineProgressBar(LocalDate.of(2025, 6, 11), LocalDate.of(2025, 7, 11))
             }
         }
@@ -182,8 +186,8 @@ fun EventIndicator(
 
 @Composable
 fun RiskLevelIndicater(
-    eventRisk:String
-){
+    eventRisk: String
+) {
     Box(
         modifier = Modifier
             .background(
@@ -202,7 +206,7 @@ fun RiskLevelIndicater(
 }
 
 fun getColor(eventRisk: String): Color {
-    return when(eventRisk.lowercase()) {
+    return when (eventRisk.lowercase()) {
         "low" -> Color(0xFF2FE843)    // Green
         "medium" -> Color(0xFF2C66C0) // Yellow
         "high" -> Color(0xFFF44336)   // Red
@@ -220,7 +224,7 @@ fun DeadlineProgressBar(startDate: LocalDate, endDate: LocalDate) {
     // Calculate progress (0.0 to 1.0)
     val totalDays = ChronoUnit.DAYS.between(startDate, endDate).toFloat()
     val daysPassed = ChronoUnit.DAYS.between(startDate, currentDate).toFloat()
-    val progress =  0.4f//(daysPassed / totalDays).coerceIn(0f, 1f)
+    val progress = 0.4f//(daysPassed / totalDays).coerceIn(0f, 1f)
 
     // Custom progress bar with shadow and rounded corners
     Surface(
